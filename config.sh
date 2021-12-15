cd8f6bd39a9f8bfd5db9adb255d35547dfe088eb
Daiwei Song <daiwei.song@tusimple.ai>
2021-12-15 18:33:40 +0800

    add

--- config.sh
+++ config.sh
@@ -1,60 +1,28 @@
 #!/bin/sh
+sudo apt install -y git terminator nfs-common openssh-server zsh python3-pip curl flameshot
+git config --global user.email "daiwei.song@tusimple.ai" 
+git config --global user.name "Daiwei Song"
 
-set -e
-
-sudo apt-get install zsh
 chsh -s /bin/zsh
 
-
-echo "====================="
-echo "1. backup your config files. e.g. ~/.zshrc -> ~/.zshrc.backup"
-# Require Vim with Lua
-if [ -f "$HOME/.zshrc" ]
-then
- echo "$file found."
- mv ~/.zshrc ~/.zshrc.backup
-else
- echo "$file not found."
-fi
-
-echo "====================="
-echo "2. install on-my-zsh"
 
 sudo pip install flake8
 sudo mkdir -p ~/.vim_runtime
 sudo mkdir -p ~/.oh-my-zsh
 sudo mkdir -p ~/.config
-cp -R vim/* ~/.vim_runtime/
-# rsync -av --progress vim ~/.vim_runtime
+sudo cp -R vim/* ~/.vim_runtime/
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-cp -R oh-my-zsh/* ~/.oh-my-zsh/
+sudo cp -R oh-my-zsh/* ~/.oh-my-zsh/
 chsh -s /bin/zsh
 cp oh-my-zsh/.zshrc ~/
 cp flake8 ~/.config/flake8
 
 echo "====================="
-echo "3. config Vim"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || true
-pip install autopep8 --user
-vim +PluginInstall +qall
-git config --global diff.tool vimdiff
-git config --global difftool.prompt false
-git config --global alias.d difftool
-(cd ~/.vim/bundle/YouCompleteMe && ./install.py)
 
-echo "====================="
+sudo apt-get install aptitude 
 
-echo "4. install Typora"
-# optional, but recommended
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
-# add Typora's repository
-sudo add-apt-repository 'deb https://typora.io/linux ./'
-sudo apt-get update
-# install typora
-sudo apt-get -y install typora
-echo "====================="
+sudo aptitude install fcitx-bin fcitx-table fcitx-config-gtk fcitx-frontend-all 
 
-echo "5. install ssh server"
-sudo apt-get -y install openssh-server 
-echo "====================="
+ # flameshot gui
+
 echo "6. Success!"
---
+++ gpu.sh
@@ -0,0 +1,7 @@
+ubuntu-drivers devices 
+sudo apt install nvidia-driver-470
+
+# cuda:
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=runfile_local
+# cudnn:
+# https://developer.nvidia.com/rdp/cudnn-download
