c95c60a1b870197361235f0096d7be5367725f73
Daiwei Song <songdaiwei@hesaitech.com>
2023-10-26 19:49:03 +0800

    1

--- config.sh
+++ config.sh
@@ -3,10 +3,13 @@
 password=$1
 
 # apt
-sudo apt install -y git terminator nfs-common openssh-server zsh python3-pip curl flameshot tmux cloudcompare
+echo $password | sudo -S apt install -y git terminator nfs-common openssh-server zsh python3-pip curl flameshot tmux cloudcompare wireshark
+# printer
+# echo $password | sudo -S apt install samba-client python3-smbc smbclient
+# echo $password | sudo -Ssystemctl restart cups.service
+
 
 # git
-
 git config --global user.email "daiwei.song@outlook.com"
 git config --global user.name "Daiwei Song"
 
@@ -24,20 +27,17 @@ mkdir ~/.config/terminator/
 cp file/terminator_config ~/.config/terminator/config
 
 
-
+# flake8
 cp file/flake8 ~/.config/flake8
 
 # vim
-sudo mkdir -p ~/.vim_runtime
-sudo cp -R vim/* ~/.vim_runtime/
+echo $password | sudo -S mkdir -p ~/.vim_runtime
+echo $password | sudo -S cp -R vim/* ~/.vim_runtime/
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-echo "====================="
-
 # input
-sudo apt-get install aptitude
-
-sudo aptitude install fcitx-bin fcitx-table fcitx-config-gtk fcitx-frontend-all
+echo $password | sudo -S apt-get install -y aptitude
+echo $password | sudo -S aptitude install fcitx-bin fcitx-table fcitx-config-gtk fcitx-frontend-all
 
 # flameshot gui
 
@@ -45,12 +45,14 @@ echo "6. Success!"
 
 # indicator-sysmonitor
 
-sudo apt-get install -y indicator-sysmonitor
+echo $password | sudo -S apt-get install -y indicator-sysmonitor
 indicator-sysmonitor &
-
 echo “set it to run on startup”
-
 echo "cpu: {cpu} gpu: {nvgpu} mem: {mem}"
 
 # python
 # pip3 install flake8
+
+# VS Code right click menu
+touch ~/.local/share/nautilus/scripts/Open-in-VSCode
+echo "code ./$1" > ~/.local/share/nautilus/scripts/Open-in-VSCode
--- gpu.sh
+++ gpu.sh
@@ -2,6 +2,6 @@ ubuntu-drivers devices
 sudo apt install nvidia-driver-470
 
 # cuda:
-# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=runfile_local
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=runfile_local
 # cudnn:
 # https://developer.nvidia.com/rdp/cudnn-download
